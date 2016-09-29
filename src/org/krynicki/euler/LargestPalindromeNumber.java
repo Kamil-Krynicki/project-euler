5514bc359c6c6f242a276360f541d1a576038e62
Kamil-Krynicki <kamil.krynicki@gmail.com>
2016-09-29 13:05:02 +0200

    Problem 4: largest palindrome. I don't like the brute-force, but yet... here I am.

--- src/org/krynicki/euler/LargestPalindromeNumber.java
+++ src/org/krynicki/euler/LargestPalindromeNumber.java
@@ -1,5 +1,9 @@
 package org.krynicki.euler;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by kamil.krynicki on 29/09/2016.
  */
@@ -7,47 +11,71 @@ public class LargestPalindromeNumber {
     /**
      * A palindromic number reads the same both ways.
      * The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-     *
+     * <p>
      * Find the largest palindrome made from the product of two 3-digit numbers.
      */
 
-    int invert(int s) {
-        int result = 0;
+    int palindromize(int s) {
+        int result = s;
 
-        while(s!=0) {
-            result+=s%10;
-            result*=10;
-            s/=10;
-        }
-
-        return (result/10);
-    }
-
-    int length(int s){
-        int result = 0;
-
-        while(s!=0) {
-            s/=10;
-            result++;
+        while (s != 0) {
+            result *= 10;
+            result += s % 10;
+            s /= 10;
         }
 
         return result;
     }
 
-    int glue(int s1, int s2) {
-        return (int) (s1*Math.pow(10, length(s2))+s2);
+    void generatePalindromeNumbers(int digit) {
+        if (digit % 2 == 0) {
+            digit /= 2;
+        } else {
+            digit /= 2;
+        }
+
+        int max = (int) Math.pow(10, digit) - 1;
+        int min = (int) Math.pow(10, digit - 1);
+
+        int curr = max;
+
+        while (curr >= min) {
+            System.out.println(palindromize(curr));
+            curr--;
+        }
     }
 
-    void generatePalindromeNumbers(short digit) {
-        int max = (int) Math.pow(10, length(digit) - 1);
-        int min = (int) Math.pow(10, length(digit - 1));
+    int invert(int s) {
+        int result = 0;
+
+        while (s != 0) {
+            result += s % 10;
+            result *= 10;
+            s /= 10;
+        }
+
+        return (result / 10);
+    }
+
+    boolean isPalindrome(int i) {
+        return i == invert(i);
     }
 
 
     static public void main(String[] args) {
         LargestPalindromeNumber t = new LargestPalindromeNumber();
-        short s1 = (short) 542;
-        System.out.println(t.glue(s1, t.invert(s1)));
+
+        Set<Integer> result = new HashSet<>();
+
+        for (int i = 100; i < 1000; i++) {
+            for (int j = 100; j < 1000; j++) {
+                if (t.isPalindrome(i * j)) {
+                    result.add(i*j);
+                }
+            }
+        }
+
+        System.out.println(Collections.max(result));
     }
 
 }
