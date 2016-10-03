d72d6410b0e402f1e71d9f8390e5e25468cbb42f
Kamil-Krynicki <kamil.krynicki@gmail.com>
2016-10-03 09:41:31 +0200

    Problem 7:10001th prime

---
+++ src/org/krynicki/euler/NthPrime.java
@@ -0,0 +1,42 @@
+package org.krynicki.euler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kamil.krynicki on 03/10/2016.
+ */
+public class NthPrime {
+    private List<Long> primes;
+    public NthPrime(int n) {
+        primes = new ArrayList<>(n);
+
+        primes.add(2l);
+
+        long i = 3;
+
+        while(primes.size()<n) {
+            if(!isDivisible(i, primes)) {
+                primes.add(i);
+            }
+            i+=2;
+        }
+    }
+
+    long get() {
+        return primes.get(primes.size()-1);
+    }
+
+    private boolean isDivisible(long i, List<Long> primes) {
+        for(Long prime:primes) {
+            if(i%prime==0) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        NthPrime t = new NthPrime(10001);
+
+        System.out.println(t.get());
+    }
+}
