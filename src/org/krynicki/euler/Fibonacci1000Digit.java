3cf208bc2ea36b2842f46bab7e014fa4914cb7e9
Kamil-Krynicki <kamil.krynicki@gmail.com>
2016-10-17 22:51:54 +0200

    Problem 25: a mini-problem, really.

---
+++ src/org/krynicki/euler/Fibonacci1000Digit.java
@@ -0,0 +1,41 @@
+package org.krynicki.euler;
+
+import java.math.BigInteger;
+
+/**
+ * Created by K on 2016-10-17.
+ */
+public class Fibonacci1000Digit {
+
+    public static void main(String[] args) {
+        long t1 = System.currentTimeMillis();
+        FibonacciFinder f = new FibonacciFinder();
+
+        int i = 3;
+
+        while(f.next().toString().length() < 1000) {
+            i++;
+        }
+
+        System.out.println(i);
+
+        long t2 = System.currentTimeMillis();
+        System.out.println(t2 - t1);
+    }
+
+    static class FibonacciFinder {
+        BigInteger prvFibA = BigInteger.ONE;
+        BigInteger prvFibB = BigInteger.ONE;
+
+        BigInteger next() {
+            BigInteger next = prvFibA.add(prvFibB);
+            prvFibA = prvFibB;
+            prvFibB = next;
+            return next;
+        }
+
+        BigInteger current() {
+            return prvFibB;
+        }
+    }
+}
