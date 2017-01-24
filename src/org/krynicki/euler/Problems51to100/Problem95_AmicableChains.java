d594903fef7b282491d868d08027c52d6e9b19c3
Kamil-Krynicki <kamil.krynicki@gmail.com>
2017-01-24 18:32:49 +0100

    Problem 95: mine worked in 2 sec. With the new sieve it's 150 ms.

--- src/org/krynicki/euler/Problems51to100/Problem95_AmicableChains.java
+++ src/org/krynicki/euler/Problems51to100/Problem95_AmicableChains.java
@@ -1,6 +1,9 @@
 package org.krynicki.euler.Problems51to100;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  * Created by K on 2017-01-23.
@@ -8,7 +11,8 @@ import java.util.Arrays;
 public class Problem95_AmicableChains {
 
     private static int max;
-    private static int[] chains;
+    private static int[] checked;
+    private static int[] divisorSums;
 
     /*
             The proper divisors of a number are all the divisors excluding the number itself. For example, the proper divisors
@@ -28,52 +32,52 @@ public class Problem95_AmicableChains {
     public static void main(String[] args) {
         long t1 = System.currentTimeMillis();
 
-        max = (int) 1E4;
-        chains = new int[max];
-        chains[0] = -1;
+        max = (int) 1E6;
+        checked = new int[max];
+        divisorSums = new int[max];
 
+        Arrays.fill(divisorSums, 1);
 
-        for(int i = 1210; i< max; i++){
-            if(chains[i]==0) markChain(i);
+        sieve();
+
+        for (int i = 1; i < max; i++) {
+            if (checked[i] == 0) {
+                markChain(i);
+            }
         }
-
-        System.out.println(Arrays.toString(chains));
 
         long t2 = System.currentTimeMillis();
         System.out.println(t2 - t1);
     }
 
     private static void markChain(int start) {
-        chains[start] = innerMarkChain(start, 1);
-    }
+        Deque<Integer> chain = new LinkedList<>();
 
-    private static int innerMarkChain(int current, int depth) {
-        if(chains[current] == 0) {
-            chains[current] = depth;
-            int next = sumOfDivisors(current);
-            if (next <= max) {
-                return (chains[current] = innerMarkChain(next, depth+1));
-            } else {
-                return (chains[current] = -1);
-            }
-        } else if(chains[current] > 0) {
-            return depth + chains[current] - 1;
-        } else {
-            return -1;
+        int next = start;
+
+        while (checked[next]==0) {
+            chain.push(next);
+            checked[next] = start;
+            next = divisorSums[next];
+            if (next >= max) return;
+        }
+
+        if(checked[next] == start) {
+           chain.push(next);
+           while(!chain.peekFirst().equals(chain.pollLast()));
+           System.out.println(Collections.min(chain) + " : " + (chain.size() - 1));
         }
     }
 
-    public static int sumOfDivisors(int val) {
-        int div = 1;
-        int result = 1;
-
-        while(++div <= Math.sqrt(val)) {
-            if (val % div == 0) {
-                result += div;
-                result += val / div;
+    // stole this one. It's good
+    private static void sieve() {
+        for(int n = 2; n < divisorSums.length; n++) {
+            if(divisorSums[n] != 1) {divisorSums[n] -= n; continue;}
+            for(int m = n + n; m < divisorSums.length; m += n) {
+                int pow = n, factor = 1;
+                while(m % pow == 0) {factor += pow; pow *= n;}
+                divisorSums[m] *= factor;
             }
         }
-
-        return result;
     }
 }
