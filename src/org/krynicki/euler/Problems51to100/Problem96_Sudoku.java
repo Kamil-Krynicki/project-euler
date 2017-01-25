973401f8d7f50d2d5511ddf502e0f36b8abc41d5
Kamil-Krynicki <kamil.krynicki@gmail.com>
2017-01-25 22:05:14 +0100

    Problem 96: pretty now

--- src/org/krynicki/euler/Problems51to100/Problem96_Sudoku.java
+++ src/org/krynicki/euler/Problems51to100/Problem96_Sudoku.java
@@ -2,9 +2,9 @@ package org.krynicki.euler.Problems51to100;
 
 import com.google.common.base.Preconditions;
 
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 /**
@@ -25,11 +25,11 @@ public class Problem96_Sudoku {
         SudokuModel m = new SudokuModel(9, 3);
 
         int x = 0;
-        for(String row: sudokuSetup.split(",")) {
+        for (String row : sudokuSetup.split(",")) {
             int y = 0;
-            for(char c : row.toCharArray()) {
+            for (char c : row.toCharArray()) {
                 int value = c - '0';
-                if(value>0) m.put(value, x, y);
+                if (value > 0) m.put(value, x, y);
                 y++;
             }
             x++;
@@ -44,108 +44,95 @@ public class Problem96_Sudoku {
     }
 
     static class SudokuModel {
-        private int emptySpaces;
-        private Map<Integer, SudokuLayer> layers;
-        private int[][] board;
+        private final Set<SudokuLayer> layers;
+        private final int[][] board;
 
-        private int size;
-        private int blockSize;
-        private final String hRule;
+        private int emptySpaces;
 
         public SudokuModel(int size, int blockSize) {
+            Preconditions.checkArgument(size > 0);
+            Preconditions.checkArgument(blockSize > 0);
+            Preconditions.checkArgument(blockSize < size);
+
             this.board = new int[size][size];
+            this.emptySpaces = size * size;
 
-            this.layers = new HashMap<>();
-            for(int i = 1; i<10; i++) {
-                this.layers.put(i, new SudokuLayer(size, blockSize));
-            }
+            this.layers = new HashSet<>();
 
-            this.size = size;
-            this.blockSize = blockSize;
-
-            this.hRule = makeHrule(size);
-            this.emptySpaces = size*size;
-        }
-
-        private String makeHrule(int size) {
-            StringBuilder b = new StringBuilder();
-            for(int i=0;i<=2*size;i++){
-                b.append('-');
-            }
-            return b.toString();
+            for (int i = 1; i < 10; i++)
+                this.layers.add(new SudokuLayer(size, blockSize, i));
         }
 
         public void put(int value, int atX, int atY) {
-            for(Map.Entry<Integer, SudokuLayer> layer : layers.entrySet()) {
-                if(layer.getKey() != value)
-                    layer.getValue().occupy(atX, atY);
+            Preconditions.checkArgument(atX >= 0);
+            Preconditions.checkArgument(atY >= 0);
+
+            Preconditions.checkArgument(atX < board.length);
+            Preconditions.checkArgument(atY < board[0].length);
+
+            for (SudokuLayer layer : layers) {
+                if (layer.symbol() != value)
+                    layer.occupy(atX, atY);
                 else
-                    layer.getValue().put(atX, atY);
+                    layer.put(atX, atY);
             }
+
             board[atX][atY] = value;
             emptySpaces--;
         }
 
         public boolean isSolved() {
-            return emptySpaces==0;
+            return emptySpaces == 0;
         }
 
-
         public void solve() {
-            while (!isSolved()) {
-                Coords coords;
-                boolean found = false;
-                for(Map.Entry<Integer, SudokuLayer> value : layers.entrySet()) {
-                    coords = value.getValue().nextStep();
-                    if(coords.isValid()) {
-                        put(value.getKey(), coords.x, coords.y);
-                        found = true;
+            Coords coords = null;
+
+            do {
+                for (SudokuLayer layer : layers) {
+                    coords = layer.nextStep();
+                    if (coords != null) {
+                        put(layer.symbol(), coords.x, coords.y);
                         break;
                     }
                 }
+            } while (!isSolved() && coords != null);
 
-                if(!found) {
-                    System.out.print("Unable to make progress from:");
-                    System.out.print(this);
-                    break;
-                }
+            if (coords == null) {
+                System.out.print("Unable to make progress from:");
+                System.out.print(this);
             }
-        }
-
-        public Coords nextStep(int value) {
-            return layers.get(value).nextStep();
         }
 
         @Override
         public String toString() {
             StringBuilder b = new StringBuilder();
 
-            for(int[] boardRow : board){
-                b.append(hRule);
-                b.append('\n');
-                for(int field : boardRow){
+            for (int[] boardRow : board) {
+                for (int field : boardRow) {
                     b.append('|');
-
-                    if(field == 0) {
-                        b.append(' ');
-                    } else {
-                        b.append(field);
-                    }
+                    if (field == 0) b.append(' ');
+                    else b.append(field);
                 }
                 b.append("|\n");
             }
-            b.append(hRule);
 
             return b.toString();
         }
     }
 
     static class SudokuLayer {
-        private boolean[][] occupied;
-        private int size = 9;
-        private int blockSize = 3;
+        private final int symbol;
+        private final boolean[][] occupied;
+        private final int size;
+        private final int blockSize;
 
-        public SudokuLayer(int size, int blockSize) {
+        SudokuLayer(int size, int blockSize, int symbol) {
+            Preconditions.checkArgument(size > 0);
+            Preconditions.checkArgument(blockSize > 0);
+            Preconditions.checkArgument(blockSize < size);
+
+            this.symbol = symbol;
             this.occupied = new boolean[size][size];
             this.size = size;
             this.blockSize = blockSize;
@@ -157,123 +144,100 @@ public class Problem96_Sudoku {
             Preconditions.checkArgument(x >= 0);
             Preconditions.checkArgument(y >= 0);
 
-            Preconditions.checkArgument(canPut(x, y));
+            Preconditions.checkArgument(isUnoccupied(x, y));
 
             markColumn(x);
             markRow(y);
             markBlock(x, y);
         }
 
+        public int symbol() {
+            return symbol;
+        }
+
         public void occupy(int x, int y) {
+            Preconditions.checkArgument(x < size);
+            Preconditions.checkArgument(y < size);
+            Preconditions.checkArgument(x >= 0);
+            Preconditions.checkArgument(y >= 0);
+
             occupied[x][y] = true;
         }
 
         public Coords nextStep() {
-            for (int x = 0; x < size; x++) {
-                if(hasUniquePlacementColumn(x)) {
+            for (int x = 0; x < size; x++)
+                if (hasUniquePlacementColumn(x))
                     return getPlaceInColumn(x);
-                }
-            }
 
-            for (int y = 0; y < size; y++) {
-                if(hasUniquePlacementRow(y)) {
+            for (int y = 0; y < size; y++)
+                if (hasUniquePlacementRow(y))
                     return getPlaceInRow(y);
-                }
-            }
 
-            for (int y = 0; y < size; y += blockSize) {
-                for (int x = 0; x < size; x += blockSize) {
-                    Coords result = canPlaceUniquelyInBlock(x, y);
-                    if (result.isValid()) {
-                        return result;
-                    }
-                }
-            }
+            for (int y = 0; y < size; y += blockSize)
+                for (int x = 0; x < size; x += blockSize)
+                    if (hasUniquePlacementInBlock(x, y))
+                        return canPlaceUniquelyInBlock(x, y);
 
-            return Coords.INVALID;
+            return null;
         }
 
         public boolean hasUniquePlacementRow(int y) {
-            return IntStream.range(0, size).filter( t -> isUnoccupied(t, y)).count() == 1;
+            return IntStream.range(0, size).filter(t -> isUnoccupied(t, y)).count() == 1;
         }
 
         private Coords getPlaceInRow(int y) {
-            return new Coords(IntStream.range(0, size).filter( t -> isUnoccupied(t, y)).findFirst().getAsInt(), y);
+            return new Coords(IntStream.range(0, size).filter(t -> isUnoccupied(t, y)).findFirst().getAsInt(), y);
         }
 
         public boolean hasUniquePlacementColumn(int x) {
-            return IntStream.range(0, size).filter( t -> isUnoccupied(x, t)).count() == 1;
+            return IntStream.range(0, size).filter(t -> isUnoccupied(x, t)).count() == 1;
         }
 
         private Coords getPlaceInColumn(int x) {
-            return new Coords(x, IntStream.range(0, size).filter( t -> isUnoccupied(x, t)).findFirst().getAsInt());
+            return new Coords(x, IntStream.range(0, size).filter(t -> isUnoccupied(x, t)).findFirst().getAsInt());
         }
 
         private boolean isUnoccupied(int x, int y) {
             return !occupied[x][y];
         }
 
-        private boolean isUnoccupied(Coords coords) {
-            return !occupied[coords.x][coords.y];
+        public boolean hasUniquePlacementInBlock(int x, int y) {
+            int xOffset = x / (size / blockSize) * blockSize;
+            int yOffset = y / (size / blockSize) * blockSize;
+
+            return IntStream.range(xOffset, xOffset + blockSize)
+                    .flatMap(t -> IntStream.range(yOffset, yOffset + blockSize)
+                            .filter(u -> isUnoccupied(t, u))).count() == 1;
         }
 
         private Coords canPlaceUniquelyInBlock(int x, int y) {
             int xOffset = x / (size / blockSize) * blockSize;
             int yOffset = y / (size / blockSize) * blockSize;
 
-            Coords result = Coords.INVALID;
-
-            for (int i = xOffset; i < xOffset + blockSize; i++)
-                for (int j = yOffset; j < yOffset + blockSize; j++)
-                    if (isUnoccupied(i, j))
-                        if (!result.isValid()) result = new Coords(i, j);
-                        else return Coords.INVALID;
-
-            return result;
+            return IntStream.range(xOffset, xOffset + blockSize)
+                    .boxed()
+                    .flatMap(t -> IntStream.range(yOffset, yOffset + blockSize)
+                            .filter(u -> isUnoccupied(t, u))
+                            .mapToObj(u -> new Coords(t, u)))
+                    .findAny()
+                    .get();
         }
 
-        class RowIterator implements Iterator<Coords> {
-            private int x;
-            private int y;
+        private void markRow(int y) {
+            IntStream.range(0, size).forEach(t -> occupy(t, y));
+        }
 
-            private int max;
-
-            public RowIterator(int y, int max) {
-                this.y = y;
-                this.x = 0;
-                this.max = max;
-            }
-
-            @Override
-            public boolean hasNext() {
-                return x < max;
-            }
-
-            @Override
-            public Coords next() {
-                return new Coords(x++, y);
-            }
+        private void markColumn(int x) {
+            IntStream.range(0, size).forEach(t -> occupy(x, t));
         }
 
         private void markBlock(int x, int y) {
             int xOffset = x / (size / blockSize) * blockSize;
             int yOffset = y / (size / blockSize) * blockSize;
 
-            for (int i = xOffset; i < xOffset + blockSize; i++)
-                for (int j = yOffset; j < yOffset + blockSize; j++)
-                    occupy(i, j);
-        }
-
-        private void markRow(int y) {
-            for (int i = 0; i < size; i++) occupy(i, y);
-        }
-
-        private void markColumn(int x) {
-            for (int i = 0; i < size; i++) occupy(x, i);
-        }
-
-        private boolean canPut(int x, int y) {
-            return isUnoccupied(x, y);
+            IntStream.range(xOffset, xOffset + blockSize)
+                    .forEach(t -> IntStream.range(yOffset, yOffset + blockSize)
+                            .forEach(u -> occupy(t, u)));
         }
 
         @Override
@@ -290,41 +254,14 @@ public class Problem96_Sudoku {
         }
     }
 
+
     static class Coords {
-        public final static Coords INVALID = new Coords() {
-            @Override
-            public boolean isValid() {
-                return false;
-            }
-
-            public String toString() {
-                return "Coords{INVALID}";
-            }
-        };
-
         public final int x;
         public final int y;
-
-        public Coords() {
-            this.x = Integer.MIN_VALUE;
-            this.y = Integer.MIN_VALUE;
-        }
 
         public Coords(int x, int y) {
             this.x = x;
             this.y = y;
-        }
-
-        public boolean isValid() {
-            return x >= 0 && y >= 0;
-        }
-
-        @Override
-        public String toString() {
-            return "Coords{" +
-                    "x=" + x +
-                    ", y=" + y +
-                    '}';
         }
     }
 }
